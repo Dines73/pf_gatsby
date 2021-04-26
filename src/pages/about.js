428f662825ca9e83d8147f7dcc91df6b0137a510
edin <edo.truman@gmail.com>
2021-04-26 17:10:15 +0200

    Initial

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id harum
+          minus facere quisquam quas debitis eius voluptatem quaerat ullam,
+          cupiditate, velit optio tempora ratione. Architecto excepturi harum
+          accusantium mollitia laboriosam!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          alias obcaecati aliquam explicabo eligendi delectus blanditiis
+          repellat rerum molestiae est eum quia distinctio incidunt aspernatur,
+          unde optio nesciunt earum quas.
+        </p>
+      </div>
+    </Layout>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function Home() {
+  return (
+    <Layout>
+      <section>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & deploy</h3>
+          <p>UX Designer & Web Developer based in Vienna</p>
+        </div>
+      </section>
+    </Layout>
+  )
+}
