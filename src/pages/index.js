5b3f75c303cbbd9e0619b046e43abf7603d7c1a7
edin <edo.truman@gmail.com>
2021-04-27 20:20:13 +0200

    images

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
+import * as neumor from "../styles/neumor.module.css"
 
 export default function Home() {
   return (
@@ -15,6 +16,13 @@ export default function Home() {
             Mein Werk
           </Link>
         </div>
+        <div className={neumor.scene}>
+          <div className={neumor.parent}>
+            CSS IS
+            <span className={neumor.awesome}>AWESOME</span>
+          </div>
+        </div>
+        {/* <img src="/coder.svg" alt="coder banner" style={{ maxWidth: "100%" }} /> */}
       </section>
     </Layout>
   )
